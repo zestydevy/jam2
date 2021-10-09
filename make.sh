0ac10bd5f32d9a7fcf063c7467a1d3aabd2bd815
miluaces <miluaces@yahoo.com>
2021-10-09 16:42:09 -0700

    Fix make.sh

--- make.sh
+++ make.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-export PATH="$HOME/n64chain/bin:$PATH"
-export N64_LIBGCCDIR="$HOME/n64chain/lib/gcc/mips64-elf/10.2.0"
-export N64_NEWLIBDIR="$HOME/n64chain/mips64-elf/lib"
-export N64_NEWLIBINCDIR="$HOME/n64chain/mips64-elf/include"
+export ROOT=/etc/n64
+export PATH="/opt/crashsdk/bin:$PATH"
+export N64_LIBGCCDIR="/opt/crashsdk/lib/gcc/mips64-elf/11.2.0"
+export N64_NEWLIBDIR="/opt/crashsdk/mips64-elf/lib"
+export N64_NEWLIBINCDIR="/opt/crashsdk/mips64-elf/include"
 export ROOT=/etc/n64
 
 rm -rf ./build
